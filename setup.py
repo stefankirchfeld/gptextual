8e5636ba89186f4655543111b8072ef759128c81
stefankirchfeld <stefan.kirchfeld@gmail.com>
2024-02-21 17:01:11 -0800

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gptextual",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(exclude=["tests"]),
     package_data={"": ["*.tcss"]},
     include_package_data=True,
